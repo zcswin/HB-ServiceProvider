07767094ce2b9ce996f3e28bf056ffe568250823
zcswin <zcswin@qq.com>
2025-04-06 21:17:46 +0800

    Dynamic Port Configuration ADD QMS

--- src/main/java/charles/zhou/serviceprovider/controller/ProviderController.java
+++ src/main/java/charles/zhou/serviceprovider/controller/ProviderController.java
@@ -3,6 +3,7 @@ package charles.zhou.serviceprovider.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,14 @@ import io.swagger.v3.oas.annotations.Operation;
 @RestController
 @RequestMapping("/service-provider")
 public class ProviderController {
+	
+    @Value("${server.port}")
+    private String serverPort; // 注入当前服务的端口号
+	
     @GetMapping("/service")
     @Operation(summary = "提供服务消息", description = "返回服务消息详细")
     public String providerService() {
-        return "This is a service provided by the Charles Zhou.";
+        return "This is a service provided by the Charles Zhou.Port:" + serverPort;
     }   
 
     
